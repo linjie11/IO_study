d88154c14503a82c5d714a1a38c627814745b278
linjie11 <linjieli_uestc@163.com>
2020-08-17 23:15:34 +0800

    抛出异常

--- src/cn/linjie/ExceptionDemo1.java
+++ src/cn/linjie/ExceptionDemo1.java
@@ -8,9 +8,11 @@ import java.util.Date;
     编译时异常和运行时异常的区别
     编译期异常：
     运行期异常
+
+    throw:如果出现了异常情况，我们可以把该异常抛出，这个时候的抛出应该是异常的对象
  */
 public class ExceptionDemo1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("处理异常之前");
         try {
             method();
@@ -23,7 +25,7 @@ public class ExceptionDemo1 {
 
 
     /*运行期异常的抛出*/
-    private static void method2() throws ArithmeticException{
+    private static void method2() throws ArithmeticException,Exception{
         int a = 10;
         int b = 0;
         System.out.println(a/b);
@@ -38,5 +40,15 @@ public class ExceptionDemo1 {
         Date d = null;
         d = sdf.parse(s);
         System.out.println(d);
+    }
+
+    public static void method3(){
+        int a = 10;
+        int b = 0;
+        if (b==0){
+            throw new ArithmeticException();
+        }else{
+            System.out.println(a/b);
+        }
     }
 }
